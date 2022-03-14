2f5492c1c3bea72e281a58c2e02792b5a3968781
Samapuma <79788895+SamanthaWathenOlson@users.noreply.github.com>
2022-03-14 16:45:31 -0400

    updating more infor

---
+++ API/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+import main
+
+
+@aap.route("/customer", methods=["GET"])
+
+def create_customer():
+    pass
+
+app.route("/customer/customer_id", method=["DELETE"])
+
+def delete_customer_by_id():
+    pass
